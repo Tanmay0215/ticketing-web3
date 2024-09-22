d82ff164cb08507edbf50bc1884ee40f377d923e
Ayush singla <143862828+Ayushsingla1@users.noreply.github.com>
2024-09-22 11:16:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000, 
+    chunkSizeWarningLimit: 1500, 
     outDir: 'dist',
   },
   base: '/', 
