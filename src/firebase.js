ab0932abe4768d142351f6ce52aebc2f58ca3993
Ayush singla <143862828+Ayushsingla1@users.noreply.github.com>
2024-09-22 07:25:01 +0530

    .env updated

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBk--aijc3iuCTAHcbycCe3I171ELxq1S8",
-  authDomain: "innovortex-59799.firebaseapp.com",
-  projectId: "innovortex-59799",
-  storageBucket: "innovortex-59799.appspot.com",
-  messagingSenderId: "1005335065169",
-  appId: "1:1005335065169:web:182824309eb5a3acd5fe70",
-  measurementId: "G-YPH8XD6N00"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
