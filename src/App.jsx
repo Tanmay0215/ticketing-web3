2dff870f4ad29ac80097f96407b5cdfe85175eda
Tanmay0215 <tanmaygupta.0215@gmail.com>
2025-02-19 18:32:58 +0530

    refactored and cleaned code

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,30 @@
-import { Routes,Route } from 'react-router-dom'
-import Homepage from './Pages/Homepage'
+import { Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
 import Events from './Pages/Events'
 import SignUp from './Pages/SignUp'
 import Login from './Pages/Login'
-// import BillingPage from './Pages/BillingPage'
 import NotFound from './Pages/NotFound'
 import Event from './Pages/Event'
 import Payment2 from './Pages/Payment2'
 import CreateEvent from './Pages/CreateEvent'
 import BillingPage2 from './Pages/BillingPage2'
-import About from './Pages/About'
 
 function App() {
   return (
-    <div className='w-full min-h-screen'>
+    <div className="min-h-screen bg-black font-poppins">
       <Routes>
-        <Route path = '/' element = {<Homepage/>}>
-          <Route index element = {<Home/>}/>
-          <Route path = '/Events' element = {<Events/>}/>
-          <Route path = '/Signup' element = {<SignUp/>}/>
-          <Route path = '/Login' element = {<Login/>}/>
-          <Route path = '/CreateEvent' element = {<CreateEvent/>}/>
-          <Route path = '/Billing/:id' element = {<BillingPage2/>}/>
-          <Route path = '/Event/:id' element = {<Event/>}/>
-          <Route path = '/About' element = {<About/>}/>
-          <Route path = '/Payment/:id' element = {<Payment2/>}/>
-          <Route path = '*' element = {<NotFound/>}/>
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/createEvent" element={<CreateEvent />} />
+        <Route path="/billing/:id" element={<BillingPage2 />} />
+        <Route path="/event/:id" element={<Event />} />
+        <Route path="/payment/:id" element={<Payment2 />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   )
 }
 
-export default App;
+export default App
--- src/Pages/Homepage.jsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router-dom";
-
-const HomePage = ()=> {
-     return (
-        <Outlet></Outlet>
-     )
-}
-
-export default HomePage;
