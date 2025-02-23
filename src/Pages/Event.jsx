c7479013e0e70e33a256ad355aadf88ed0c69784
Tanmay0215 <tanmaygupta.0215@gmail.com>
2025-02-23 08:09:37 +0530

    update Event page

--- src/Pages/Event.jsx
+++ src/Pages/Event.jsx
@@ -1,61 +1,92 @@
-import EventElement from "../Components/EventElement";
-import { useParams } from 'react-router-dom';
-import { useEffect, useState } from "react";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../firebase";
-import Loader from "../Components/Loader";
-import Chat from "../Components/Chat";
+import { NavLink, useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from '../firebase'
+import Loader from '../Components/Loader'
+import Chat from '../Components/Chat'
 
 const Event = () => {
-    const [item, setItem] = useState({});
-    const { id } = useParams();
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true)
+  const [item, setItem] = useState({})
+  const { id } = useParams()
 
-    console.log(id); 
-
-    useEffect(() => {
-        const fetchEvents = async () => {
-            setLoading(true);
-            try {
-                const eventsCollection = collection(db, 'EventsInfo');
-                const querySnapshot = await getDocs(eventsCollection);
-                const eventsData = querySnapshot.docs.map((doc) => ({
-                    id: doc.id,
-                    ...doc.data(),
-                }));
-                const eventIndex = parseInt(id) - 1;
-                if (eventIndex >= 0 && eventIndex < eventsData.length) {
-                    setItem(eventsData[eventIndex]);
-                } else {
-                    console.error('Event ID is out of range');
-                }
-            } catch (error) {
-                console.error('Error fetching events:', error);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchEvents();
-    }, [id]);
-
-    if (loading) {
-        return (
-            <div className="w-screen h-screen bg-black flex justify-center items-center">
-                <Loader />
-            </div>
-        );
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true)
+      try {
+        const eventsCollection = collection(db, 'EventsInfo')
+        const querySnapshot = await getDocs(eventsCollection)
+        const eventsData = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }))
+        const eventIndex = parseInt(id) - 1
+        if (eventIndex >= 0 && eventIndex < eventsData.length) {
+          setItem(eventsData[eventIndex])
+        } else {
+          console.error('Event ID is out of range')
+        }
+      } catch (error) {
+        console.error('Error fetching events:', error)
+      } finally {
+        setLoading(false)
+      }
     }
+
+    fetchEvents()
+  }, [id])
+
+  if (loading) {
     return (
-        <div>
-            <div className="bg-black w-screen h-screen overflow-hidden">
-                <EventElement item={item} />
-            </div>
-            <div className="fixed bottom-3 right-6">
-                <Chat/>
-            </div>
+      <div className="h-screen bg-black flex justify-center items-center">
+        <Loader />
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <div className="flex">
+        <div className="relative w-1/2 flex justify-end">
+          <div className="absolute inset-0 bg-gradient-to-l from-black to-transparent"></div>
+          <img
+            src={item.Image}
+            alt="Error"
+            className="h-screen aspect-2/3 object-cover"
+          />
         </div>
-    );
+        <div className="max-h-screen flex flex-col gap-y-6 w-1/2 justify-center">
+          <div className="text-[#FF9011] text-4xl font-semibold uppercase 2xl:text-6xl">
+            {item.EventName}
+          </div>
+          <div className="text-gray-200 max-w-lg leading-relaxed 2xl:text-lg">
+            {item.Description}
+          </div>
+          <div className="text-gray-200 flex flex-col gap-y-2">
+            <p className="2xl:text-2xl">
+              Artist : <span className="text-[#00FF6A]">{item.Artist}</span>
+            </p>
+            <p className="2xl:text-2xl">
+              Venue : <span className="text-[#00FF6A]">{item.Venue}</span>
+            </p>
+            <p className="2xl:text-2xl">
+              Date : <span className="text-[#00FF6A]">{item.Date}</span>
+            </p>
+            <p className="2xl:text-2xl">
+              Price : <span className="text-[#00FF6A]">{item.Price}</span>
+            </p>
+            <button className="bg-[#FF9011] w-40 h-10 rounded-md mt-4 text-black font-semibold 2xl:text-xl hover:scale-105 transition-all">
+              <NavLink to={`/Billing/${item.id}`}>BOOK NOW!!</NavLink>
+            </button>
+            <button className="bg-[#FF9011] w-40 h-10 rounded-md mt-4 text-black font-semibold 2xl:text-xl">
+              <NavLink to={`/Events`}>Go Back</NavLink>
+            </button>
+          </div>
+        </div>
+      </div>
+      <Chat />
+    </div>
+  )
 }
 
-export default Event;
+export default Event
